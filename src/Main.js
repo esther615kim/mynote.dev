c0fb45e80c59d417a15827211bf9b3460dea2c8f
esther615kim <75744588+esther615kim@users.noreply.github.com>
2021-09-28 08:24:33 +0900

    created Leftbar component created Leftbar component

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,21 @@
 import React from "react";
 import "./styles.css";
 import Navbar from "./components/Navbar";
-import { Container } from "@material-ui/core";
-
+import Leftbar from "./components/Leftbar";
+import { Container, Grid } from "@material-ui/core";
 
 export default function Main() {
   return (
     <>
       <Container />
       <Navbar />
+      <Grid container spacing={1}>
+        <Grid item sm={3}>
+          <Leftbar />
+        </Grid>
+        <Grid item sm={7}></Grid>
+        <Grid item sm={2}></Grid>
+      </Grid>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,66 @@
-import { makeStyles } from "@material-ui/core";
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
-import { MenuIcon, SearchIcon } from "@mui/material";
-import { IconButton } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  InputBase,
+  makeStyles,
+  alpha,
+  Avatar
+} from "@material-ui/core";
+import { Search, Menu } from "@material-ui/icons";
 const useStyles = makeStyles((theme) => ({
   appbar: {
     backgroundColor: theme.palette.success.light
   },
+  toolbar: {
+    display: "flex",
+    justifyContent: "space-between"
+  },
+  logoBox: {
+    display: "flex",
+    alignItems: "center"
+  },
   logoText: {
     display: "none",
+    color: "#212121",
     [theme.breakpoints.up("sm")]: {
       display: "block"
     }
+  },
+  search: {
+    display: "flex",
+    textAlign: "center",
+    alignItems: "center",
+    backgroundColor: alpha(theme.palette.common.white, 0.2),
+    borderRadius: theme.shape.borderRadius,
+    width: "40%",
+    padding: theme.spacing(1)
+  },
+  input: {
+    marginRight: theme.spacing(3)
   }
 }));
 
 const Navbar = () => {
   const classes = useStyles();
   return (
-    <AppBar className={classes.appbar} color="primary">
-      <Toolbar>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="open drawer"
-          sx={{ mr: 2 }}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography className={classes.logoText} variant="h6">
-          mynote.dev
-        </Typography>
+    <AppBar position="fixed" className={classes.appbar} color="primary">
+      <Toolbar className={classes.toolbar}>
+        <div className={classes.logoBox}>
+          <IconButton>
+            <Menu />
+          </IconButton>
+          <Typography className={classes.logoText} variant="h6">
+            mynote.dev
+          </Typography>
+        </div>
+        {/* search */}
+        <div className={classes.search}>
+          <Search />
+          <InputBase className={classes.input} placeholder=" search..." />
+        </div>
+        <Avatar />
       </Toolbar>
     </AppBar>
   );
